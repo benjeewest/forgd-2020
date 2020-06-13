ebbc5cb79f67be11a34f2dceb78485accfde3275
benjeewest <ben@benwest.me>
2020-06-13 12:31:09 +0100

    Test

--- global-templates/hero.php
+++ global-templates/hero.php
@@ -10,11 +10,10 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <div class="jumbotron bg-primary text-white">
 	<div class="container">
-		<h1 class="display-4">Forging a new Britain - together</h1>
+		<h1 class="display-4">Lorem ipsum delor set amet</h1>
 		<p class="lead">Forgd is for anyone who believes in a L</p>
 		<hr class="my-4">
-		<p>Forgd is a community of people working for a patriotic Labour Party that governs not just for itself, or the priorities of Labour members, but for the benefit of our country as a whole. For us, patriotism means so much more than flags or national anthems– it is, simply, a belief in our country, and a belief in the vast, untapped potential of all those who live in it.
-			We are patriotic not in spite of being Labour members. We are Labour members because we are patriotic.
+		<p>rtrtrtrtrtrt
 		</p>
 		<p class="lead">
 			<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
