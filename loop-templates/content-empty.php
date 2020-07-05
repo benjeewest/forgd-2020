047f64d3e222ac76e2ce35c065550808fafa745d
benjeewest <ben@benwest.me>
2020-07-05 17:39:30 +0100

    Content empty Gutenberg widths

--- loop-templates/content-empty.php
+++ loop-templates/content-empty.php
@@ -6,6 +6,14 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined( 'ABSPATH' ) || exit;?>
 
-the_content();
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<div class="entry-content">
+
+		<?php the_content(); ?>
+
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
